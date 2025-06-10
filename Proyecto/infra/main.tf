04d964f99db7e5dbf64429c97bc12c563a29f35a
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-06-10 03:39:07 -0500

    Agregar flujo de trabajo de GitHub para scraping y carga de archivos CSV en Azure Storage

--- Proyecto/infra/main.tf
+++ Proyecto/infra/main.tf
@@ -70,7 +70,6 @@ resource "azurerm_storage_account" "negociosu2_storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
-  allow_blob_public_access = false
 
   tags = {
     environment = "academic"
